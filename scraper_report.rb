30a37227521575daeaf82cb436339473f7c938d4
nwshane <django09@gmail.com>
2016-01-18 12:40:20 +0400

    Implemented ScraperReport send_email method to send email to mailcatcher.

--- scraper_report.rb
+++ scraper_report.rb
@@ -1,7 +1,21 @@
+require 'mail'
+
+Mail.defaults do
+  delivery_method :smtp, address: "localhost", port: 1025
+end
+
 class ScraperReport
   def initialize
   end
 
   def send_email
+    mail = Mail.new do
+      from    'info@jumpstart.ge'
+      to      'nathan.shane@jumpstart.ge'
+      subject 'Makler.ge Scraper Report'
+      body    'This is the body of the report'
+    end
+
+    mail.deliver!
   end
 end
