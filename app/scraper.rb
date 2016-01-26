6a06dca158816c97b780ed6a9ead38e0c8060e36
nwshane <django09@gmail.com>
2016-01-26 13:49:56 +0400

    Automatically stops scraper test run if environment is production.

--- app/scraper.rb
+++ app/scraper.rb
@@ -6,6 +6,12 @@ def run_scraper
 end
 
 def test_run_scraper
+  if environment_is_production
+    puts 'Test runs are not allowed on production environment'
+    puts 'Please change ENVIRONMENT in .env file to not be "production"'
+    exit
+  end
+  
   reset_status
   make_requests
 end
