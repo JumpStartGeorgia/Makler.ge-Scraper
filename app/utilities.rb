00a4f5ea887b70519893388689db781097de4ad6
nwshane <django09@gmail.com>
2016-02-01 16:51:40 +0400

    Changed update_github commit message.

--- app/utilities.rb
+++ app/utilities.rb
@@ -752,7 +752,7 @@ def update_github
   @log.info "updating git"
   @log.info "------------------------------"
   x = Subexec.run "git add #{@db_dump_file} #{@status_file}"
-  x = Subexec.run "git commit -m 'Updated database dump file and status.json with new makler.ge data from #{Time.now.strftime('%F')}'"
+  x = Subexec.run "git commit -m 'Updated database dump file and status.json with new makler.ge data'"
   x = Subexec.run "git push origin master"
 end
 
